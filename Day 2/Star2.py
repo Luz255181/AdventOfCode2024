12c8a7c1c488a67621e7b47f07ad29d11db466a6
Luz255181 <mira_luz_explo@hotmail.com>
2024-12-07 13:33:48 -0300

    Solucionado funcion isSafe

--- Day 2/Star2.py
+++ Day 2/Star2.py
@@ -1,12 +1,11 @@
 def isSafe(numbers):
-    length = len(numbers) - 1
-    for i in range(0, length):
-        diference = abs(int(numbers[i]) - int(numbers[i+1]))
-        if (diference > 3) or (diference == 0):
+    for i in range(0, len(numbers)-1):
+        difference = abs(int(numbers[i]) - int(numbers[i+1]))
+        if (difference > 3) or (difference < 1):
             return False
-    is_increasing = all(numbers[i] <= numbers[i + 1]
+    is_increasing = all(int(numbers[i]) <= int(numbers[i + 1])
                         for i in range(len(numbers) - 1))
-    is_decreasing = all(numbers[i] >= numbers[i + 1]
+    is_decreasing = all(int(numbers[i]) >= int(numbers[i + 1])
                         for i in range(len(numbers) - 1))
     return is_decreasing or is_increasing
 
@@ -16,15 +15,14 @@ with open("input.txt", "r") as input:
     for line in input.readlines():
         numbers = line.split()
         booleanSafe = isSafe(numbers)
-        auxiliar = numbers.copy()
         if not booleanSafe:
-            i = 0
-            while (i < (len(numbers)-2) and not booleanSafe):
-                auxiliar = numbers.copy()
-                auxiliar.pop(i)
-                booleanSafe = isSafe(auxiliar)
-                i += 1
-        if booleanSafe:
+            for i in range(len(numbers)):
+                newNumbers = numbers[:i] + numbers[i + 1:]
+                booleanSafe = isSafe(newNumbers)
+                if booleanSafe:
+                    safe += 1
+                    break
+        else:
             safe += 1
 input.close()
 print("Solucion = ", safe)
